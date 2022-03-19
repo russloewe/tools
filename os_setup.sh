30901d5b85171af5926bc215470ec4f37b57510b
Russell Loewe <russloewe@gmail.com>
2022-03-18 21:57:10 -0700

    Update os_setup.sh

--- os_setup.sh
+++ os_setup.sh
@@ -3,7 +3,7 @@
 # wget -O - https://raw.githubusercontent.com/russloewe/tools/main/os_setup.sh | bash
 
 sudo apt-get update
-sudo apt-get install -y synaptic geany geany-plugins  xfce4-goodies python3-gpg 
+sudo apt-get install -y synaptic geany geany-plugins  xfce4-goodies python3-gpg git 
 sudo wget -O ./dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb
 sudo apt-get install ./dropbox.deb
 dropbox start -i
