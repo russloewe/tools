dfea65a2d439891067a003546548a2a000f8a89d
Russell Loewe <russloewe@gmail.com>
2022-03-18 23:22:29 -0700

    Update os_setup.sh

--- os_setup.sh
+++ os_setup.sh
@@ -7,7 +7,5 @@ sudo apt-get install -y synaptic geany geany-plugins  xfce4-goodies python3-gpg 
 sudo wget -O ./dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb
 sudo apt-get install ./dropbox.deb
 dropbox start -i
-wget -c https://raw.githubusercontent.com/russloewe/tools/main/panel.tar.gz -O - | tar -xz
-rm -rf ~/.config/xfce4/panel
-mv panel ~/.config/xfce4/
+wget -O ~/panel.tar.gz https://raw.githubusercontent.com/russloewe/tools/main/panel.tar.gz 
 echo "Success"
