df5bfc2e47b0d76e5de03eb52ffa64555a5c23a6
Russell Loewe <russloewe@gmail.com>
2022-03-18 23:03:46 -0700

    Update os_setup.sh

--- os_setup.sh
+++ os_setup.sh
@@ -8,6 +8,6 @@ sudo wget -O ./dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/d
 sudo apt-get install ./dropbox.deb
 dropbox start -i
 wget -c https://raw.githubusercontent.com/russloewe/tools/main/panel.tar.gz -O - | tar -xz
-rm -r ~/.config/xfce4/panel
+rm -rf ~/.config/xfce4/panel
 mv panel ~/.config/xfce4/
 echo "Success"
