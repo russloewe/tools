87e4c9990188bb6bbde5428a160b5f5226c9ba9a
Russell Loewe <russloewe@gmail.com>
2022-03-18 19:13:04 -0700

    Update os_setup.sh

--- os_setup.sh
+++ os_setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-# wget -O - https://github.com/russloewe/tools/os_setup.sh | bash
+# wget -O - https://raw.githubusercontent.com/russloewe/tools/main/os_setup.sh | bash
 
 echo "Success"
