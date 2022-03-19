1a45fd3e2bff61449653014d2cabbce45ea4d407
Russell Loewe <russloewe@gmail.com>
2022-03-19 12:08:01 -0700

    Update os_setup.sh

--- os_setup/os_setup.sh
+++ os_setup/os_setup.sh
@@ -2,17 +2,6 @@
 
 # wget -O - https://raw.githubusercontent.com/russloewe/tools/main/os_setup/os_setup.sh | bash
 
-a=`xinput | grep Touchpad`
-regex='id=([0-9]+)'
-if [[ $a =~ $regex ]]; then
-	id=${BASH_REMATCH[1]}
-	echo "Found touchpad at device #$id"
-	echo "Disabling touchpad"
-	xinput --disable $id
-else
-	echo "No touchpad found"
-fi
-
 echo "Updating apt..."
 sudo apt-get update
 
@@ -25,7 +14,18 @@ sudo apt-get install ./dropbox.deb
 dropbox start -i
  
 echo "Setting up xfce4 panel"
-wget -O ~/xfce4-backup-panel.tar.bz2 https://raw.githubusercontent.com/russloewe/tools/main/os_setpu/xfce4-backup-panel.tar.bz2 
+wget -O ~/xfce4-backup-panel.tar.bz2 https://raw.githubusercontent.com/russloewe/tools/main/os_setup/xfce4-backup-panel.tar.bz2 
 xfce4-panel-profiles load xfce4-backup-panel.tar.bz2
+
+a=`xinput | grep Touchpad`
+regex='id=([0-9]+)'
+if [[ $a =~ $regex ]]; then
+	id=${BASH_REMATCH[1]}
+	echo "Found touchpad at device #$id"
+	echo "Disabling touchpad"
+	xinput --disable $id
+else
+	echo "No touchpad found"
+fi
 
 echo "Success"
